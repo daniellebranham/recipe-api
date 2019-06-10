8136110d87eafa043781db5db21cd14ff2278742
Danielle Branham <dxb21153@ideass-mbp.mfc.corp.mckee.com>
2019-06-10 09:37:08 -0400

    Added git ignore

--- router/routes.js
+++ router/routes.js
@@ -9,67 +9,83 @@ app.use(bodyParser.urlencoded({extended: true}));
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    User.find({}, (err, users) => {
-        if(err){
-            return res.status(500).send(err);
-        }
-        res.json(users);
-    })
+    try{
+        User.find({}, (err, users) => {
+            res.json(users);
+        });
+    }
+    catch{
+        res.status(500).send(err);
+    }
+   
 });
 
 router.get('/:id', (req, res) => {
-     User.findById(req.params.id, (err, user) => {
-        if(err){
-            return res.status(500).send(err);
-        }
-        res.json(user);
-     })
+    try{
+        User.findById(req.params.id, (err, user) => {
+            res.json(user);
+         });
+    }
+    catch{
+        res.status(500).send(err);
+    }
+     
 });
 
 router.put('/:id', (req, res) => {
-    req.user.userName = req.body.userName;
-    req.user.passWord = req.body.passWord;
-    req.user.save();
-    res.json(req.user);
+    try{
+        User.save(req.user);
+        res.json(req.user);
+    }
+    catch{
+        res.status(500).send(err);
+    }
 
-}) 
+});
 
 router.patch('/:id', (req, res) => {
-    User.findById(req.params.id, (err, user) => {
-        if(err){
-            return res.status(500).send(err);
-        }
-        if(req.body.id){
-            delete req.body.id;
-        }
-        for(let b in req.body){
-            user[b] = req.body[b];
-        }
-        user.save();
-        res.json(user);
-    })
-})
+    try{
+        User.findById(req.params.id, (err, user) => {
+            if(req.body.id){
+                delete req.body.id;
+            }
+            for(let b in req.body){
+                user[b] = req.body[b];
+            }
+            user.save();
+            res.json(user);
+        });
+    }
+    catch{
+        res.status(500).send(err);
+    }
+ 
+});
 //delete works, but the success response never comes
 router.delete('/:id', (req, res) => {
-    User.findByIdAndRemove(req.params.id, (err, user) => {
-        if(err){
-            return res.status(500).send(err);
-        }
-        return res.send.status(200);
-    })
+    try{
+        User.findByIdAndRemove(req.params.id, (err, user) => {
+            res.send.status(200);
+        });
+    }
+    catch{
+        res.status(500).send(err);
+    }
+  
 });
 
 router.post('/addUser', (req, res) => {
-    const newUser = new User(req.body);
-    newUser.userName = req.body.userName;
-    newUser.passWord = req.body.passWord;
-    newUser.save(err => {
-        if(err){
-            return res.status(500).send(err);
-        }
-    });
-    res.status(201).send(newUser);
+    try{
+        const newUser = new User();
+        newUser.userName = req.body.userName;
+        newUser.passWord = req.body.passWord;
+        User.save(newUser);
+        res.status(201).send(newUser);
+    }
+    catch{
+        res.status(500).send();
+    }
+  
 });
-
 
 module.exports = router;
