aa824438312236dcb1327e78399f1cf68823e5d3
Danielle Branham <dxb21153@ideass-mbp.mfc.corp.mckee.com>
2019-06-18 09:53:58 -0400

    Added to model and routes

--- models/models.js
+++ models/models.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId;
 
 const user = new Schema({
-    userName: { type: String },
-    passWord: { type: String },
+    Username: String,
+    Password: String,
+    Email: String,
+    Inventory: [{ Name: String, Quantity: Number }],
 });
 
 module.exports = mongoose.model('Users', user);
--- router/user-routes.js
+++ router/user-routes.js
@@ -42,15 +42,44 @@ router.delete('/:id', (req, res) => {
 
 router.post('/addUser', (req, res) => {
     try {
-        const newUser = new User({
-            userName: req.body.userName,
-            passWord: req.body.passWord,
+        User.find({ Username: req.body.Username }, (err, user) => {
+            if (user.length) {
+                return res.json({
+                    success: false,
+                    data: 'Username already exists.',
+                });
+            }
+            const newUser = new User(req.body);
+            newUser.save();
+            return res.json({ success: true, data: newUser });
         });
-        newUser.save();
-        res.json({ success: true, data: newUser });
     } catch (err) {
-        res.json({ success: false, error: err });
+        //res.json({ success: false, error: err });
+        res.status(500).send(err);
     }
+});
+
+router.get('/getInventory/:Username', (req, res) => {
+    User.find(
+        { Username: req.params.Username },
+        { Inventory: 1 },
+        (err, user) => {
+            if (err) return res.json({ success: false, error: err });
+            return res.json({ success: true, data: user });
+        }
+    );
+});
+//think about how to save inventory. lots of updates or just resave all inventory?
+router.put('/saveInventory/:id', (req, res) => {
+    User.findByIdAndUpdate(
+        { Inventory: req.params.id },
+        { Inventory: req.body },
+        { new: true },
+        (err, inventory) => {
+            if (err) return res.json({ success: false, error: err });
+            return res.json({ success: true, data: inventory });
+        }
+    );
 });
 
 module.exports = router;
